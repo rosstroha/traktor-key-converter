a21ec53562ceb4090a0dc9582f9aff28c6a16250
Ross <rtroha@netjets.com>
2016-10-31 16:21:23 -0400

    fixing complication error

--- src/TraktorKeyConverter.groovy
+++ src/TraktorKeyConverter.groovy
@@ -1,16 +1,17 @@
 import groovy.xml.StreamingMarkupBuilder
 
 class TraktorKeyConverter {
+    private final Set<String> keys = [
+            "08B", "03B", "10B", "05B", "12B", "07B", "02B", "09B",
+            "04B", "11B", "06B", "01B", "05A", "12A", "07A", "02A",
+            "09A", "04A", "11A", "06A", "01A", "08A", "03A", "10A"
+    ]
+
     public static void main(args) {
         def xmlFile = new File("collection.nml")
         println "This script needs to be in the Traktor root directory or the same location as the collection file!"
 
         //The keys in a Traktor collection file are represented by an integer 0-23, so they must remain in this order.
-        private final Set<String> keys = [
-                "08B", "03B", "10B", "05B", "12B", "07B", "02B", "09B",
-                "04B", "11B", "06B", "01B", "05A", "12A", "07A", "02A",
-                "09A", "04A", "11A", "06A", "01A", "08A", "03A", "10A"
-        ]
 
         if(xmlFile.exists()) {
             println "Collection file found!"
